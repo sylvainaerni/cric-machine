5058823fad96bb2254ca871ac6949331d03e43e7
Raphi <raphael.antonietti@liip.ch>
2019-11-05 08:20:55 +0100

    Format and function destructuring ftw

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,71 +1,81 @@
 <template>
   <div class="fixed top-0 right-0 bottom-0 left-0 flex">
-    <div class="absolute top-0 left-0 bottom-0 z-10 flex flex-col justify-between bg-green-400">
+    <div
+      class="absolute top-0 left-0 bottom-0 z-10 flex flex-col justify-between bg-green-400"
+    >
       <div class="flex flex-col items-center overflow-scroll">
         <div draggable="true" @dragstart="btnDragStart" class="mx-3">
           <draggableButton
             title="sheetsSimpleJump"
-            x="-300" y="-250"
-            width="130" height="160" />
+            x="-300"
+            y="-250"
+            width="130"
+            height="160"
+          />
         </div>
         <div draggable="true" @dragstart="btnDragStart" class="mx-3">
           <draggableButton
             title="cubeBig"
-            x="0" y="0"
-            width="300" height="250" />
+            x="0"
+            y="0"
+            width="300"
+            height="250"
+          />
         </div>
         <div draggable="true" @dragstart="btnDragStart" class="mx-3">
           <draggableButton
             title="cubeSmall"
-            x="0" y="-250"
-            width="150" height="130" />
+            x="0"
+            y="-250"
+            width="150"
+            height="130"
+          />
         </div>
         <div draggable="true" @dragstart="btnDragStart" class="mx-3">
           <draggableButton
             title="wheel001"
-            x="0" y="-570"
-            width="70" height="100" />
+            x="0"
+            y="-570"
+            width="70"
+            height="100"
+          />
         </div>
       </div>
       <div class="flex flex-col items-stretch bg-green-500 p-2">
-        <button class=" mb-1 p-2 bg-yellow-600" @click="animateAllSprites">Animate</button>
+        <button class=" mb-1 p-2 bg-yellow-600" @click="animateAllSprites">
+          Animate
+        </button>
         <button class=" mb-1 p-2 bg-green-600" @click="save">Save</button>
         <button class=" mb-1 p-2 bg-green-600" @click="load">Load</button>
       </div>
     </div>
-      <!--@mousedown="handleStageMouseDown"-->
-    <div
-      class="droptarget"
-      @dragover.prevent
-      @drop="dragDrop"
-    >
-      <v-stage
-        ref="stage"
-        :config="stageSize"
-      >
+    <!--@mousedown="handleStageMouseDown"-->
+    <div class="droptarget" @dragover.prevent @drop="dragDrop">
+      <v-stage ref="stage" :config="stageSize">
         <v-layer ref="layer">
-
           <v-group
             v-for="item in items"
             @dragstart="handleDragStart"
             @dragend="handleDragEnd"
             :key="`group-${item.itemId}`"
-            :name=item.name
-            :itemId=item.itemId
-            :x=item.x
-            :y=item.y
+            :name="item.name"
+            :itemId="item.itemId"
+            :x="item.x"
+            :y="item.y"
             ref="group"
             type="group"
             draggable="true"
           >
             <v-rect
-              :visible=item.actionsAreVisible
+              :visible="item.actionsAreVisible"
               :config="{
                 x: 0,
                 y: 0,
                 width: item.sprite.width,
                 height: item.sprite.height,
-                fill: item.actionsAreVisible ? 'rgba(255,0,0,0.2)' : 'transparent'
+                fill: item.actionsAreVisible
+                  ? 'rgba(255,0,0,0.2)'
+                  : 'transparent'
               }"
             />
             <v-sprite
@@ -74,24 +84,23 @@
               :config="item.sprite"
               ref="sprite"
               type="sprite"
-              :draggable=false
+              :draggable="false"
             />
             <v-rect
               :config="{
-                x: item.sprite.width -60,
+                x: item.sprite.width - 60,
                 y: -20,
                 width: 20,
-                height: 20,
-
+                height: 20
               }"
             />
             <v-rect
               @click="pushItemUp"
               @mouseover="handleMouseOver"
               @mouseout="handleMouseOut"
-              :visible=item.actionsAreVisible
+              :visible="item.actionsAreVisible"
               :config="{
-                x: item.sprite.width -60,
+                x: item.sprite.width - 60,
                 y: -20,
                 width: 20,
                 height: 20,
@@ -102,9 +111,9 @@
               @click="pushItemDown"
               @mouseover="handleMouseOver"
               @mouseout="handleMouseOut"
-              :visible=item.actionsAreVisible
+              :visible="item.actionsAreVisible"
               :config="{
-                x: item.sprite.width -40,
+                x: item.sprite.width - 40,
                 y: -20,
                 width: 20,
                 height: 20,
@@ -115,17 +124,16 @@
               @click="removeItem"
               @mouseover="handleMouseOver"
               @mouseout="handleMouseOut"
-              :visible=item.actionsAreVisible
+              :visible="item.actionsAreVisible"
               :config="{
-                x: item.sprite.width -20,
+                x: item.sprite.width - 20,
                 y: -20,
                 width: 20,
                 height: 20,
-                fill: 'red'}"
+                fill: 'red'
+              }"
             />
-
           </v-group>
-
         </v-layer>
       </v-stage>
     </div>
@@ -133,11 +141,9 @@
 </template>
 
 <script>
-
 const StageWidth = window.innerWidth;
 const StageHeight = window.innerHeight;
 const image = new window.Image();
-
 
 import { store } from "../Store.js";
 import DraggableButton from "@/components/DraggableButton.vue";
@@ -161,44 +167,46 @@ export default {
   },
 
   methods: {
-    btnDragStart: function(e) {
-      e.dataTransfer.setData("offsetX", e.offsetX)
-       e.dataTransfer.setData("offsetY", e.offsetY)
-       e.dataTransfer.setData("title", e.target.children[0].title)
+    btnDragStart: function({ dataTransfer, offsetX, offsetY, target }) {
+      dataTransfer.setData("offsetX", offsetX);
+      dataTransfer.setData("offsetY", offsetY);
+      dataTransfer.setData("title", target.firstElementChild.title);
     },
 
-    dragEnd: function(e) {
-
-    },
+    dragEnd: function() {},
 
     dragDrop: function(e) {
-      let xpos= e.x  - e.dataTransfer.getData("offsetX")
-      let ypos= e.y  - e.dataTransfer.getData("offsetY")
-      this.addItem({spriteName: e.dataTransfer.getData("title"), x: xpos, y: ypos})
-      document.activeElement.blur() // remove focused element
+      let xpos = e.x - e.dataTransfer.getData("offsetX");
+      let ypos = e.y - e.dataTransfer.getData("offsetY");
+      this.addItem({
+        spriteName: e.dataTransfer.getData("title"),
+        x: xpos,
+        y: ypos
+      });
+      document.activeElement.blur(); // remove focused element
     },
 
-    save(){
+    save() {
       store.save();
     },
 
-    load(){
+    load() {
       store.load();
     },
 
-    addItem(animationParam){
+    addItem(animationParam) {
       store.addItem(animationParam);
     },
 
-    removeItem(e){
+    removeItem(e) {
       store.removeItem(e.target.parent.attrs.name);
     },
 
-    pushItemUp(e){
+    pushItemUp(e) {
       store.pushItemUp(e.target.parent.attrs.name);
     },
 
-    pushItemDown(e){
+    pushItemDown(e) {
       store.pushItemDown(e.target.parent.attrs.name);
     },
 
@@ -213,10 +221,9 @@ export default {
     handleDragStart(e) {},
 
     handleDragEnd(e) {
-      console.log('e.target.parent.attrs.name', e.target)
+      console.log("e.target.parent.attrs.name", e.target);
       store.setNewPos(e.target);
     },
-
 
     reverseIndexes() {
       // just a test
@@ -224,39 +231,39 @@ export default {
       return;
     },
 
-    randomXPos : function(){
+    randomXPos: function() {
       return Math.floor(Math.random() * (StageHeight - 1 + 1)) + 1;
     },
 
-    randomYPos : function(){
+    randomYPos: function() {
       return Math.floor(Math.random() * (StageHeight - 1 + 1)) + 1;
     },
 
     animateAllSprites() {
       // animate sprite contained in items when they are added to the stage
-      console.log('animateAllSprites', this.$refs)
-      if(!this.$refs.group) return;
+      console.log("animateAllSprites", this.$refs);
+      if (!this.$refs.group) return;
       for (let i = 0; i < this.$refs.group.length; i++) {
         this.$refs.group[i].$children[1].getNode().start();
       }
     },
 
-    reloadAllImages : function(){
+    reloadAllImages: function() {
       for (let i = 0; i < store.state.items.length; i++) {
         store.state.items[i].sprite.image = image;
       }
     }
   },
 
-  beforeMount(){
+  beforeMount() {
     // at the moment, we must `addItem` here all the shapes manually,
     // otherwise it doen't draw when a new item is added.
     // TO DO: remove these four lines without breaking everything...
 
-    this.addItem({spriteName: 'cubeBig',          x: -10000, y: 0});
-    this.addItem({spriteName: 'cubeSmall',        x: -10000, y: 0});
-    this.addItem({spriteName: 'sheetsSimpleJump', x: -10000, y: 0});
-    this.addItem({spriteName: 'wheel001',         x: -10000, y: 0});
+    this.addItem({ spriteName: "cubeBig", x: -10000, y: 0 });
+    this.addItem({ spriteName: "cubeSmall", x: -10000, y: 0 });
+    this.addItem({ spriteName: "sheetsSimpleJump", x: -10000, y: 0 });
+    this.addItem({ spriteName: "wheel001", x: -10000, y: 0 });
 
     // this.addItem({spriteName: 'cubeBig',          x: this.randomXPos(), y: this.randomYPos()});
     // this.addItem({spriteName: 'cubeSmall',        x: this.randomXPos(), y: this.randomYPos()});
@@ -269,14 +276,13 @@ export default {
     image.onload = () => {
       this.reloadAllImages();
       this.animateAllSprites();
-      console.log('IMAGE IS LOADED')
+      console.log("IMAGE IS LOADED");
     };
-  },
-
+  }
 };
 </script>
 
-<style lang="postcss" >
+<style lang="postcss">
 body {
   background-color: #ccb786;
   background-image: url("https://www.transparenttextures.com/patterns/black-paper.png");
